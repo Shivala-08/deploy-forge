28fa350d6a341c9909b79936a14980dbc3cf6319
Shivala-08 <pallavdholariya@gmail.com>
2026-06-18 15:11:13 +0530

    fix: bundle public/sites in serverless function using outputFileTracingIncludes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["three"],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/sites/[siteId]/[[...slug]]': ['./public/sites/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
